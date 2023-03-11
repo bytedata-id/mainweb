000b31a87c3967e05085d3a785af0db0bbb17e17
bytedata-id <bytedata.id@gmail.com>
2023-03-12 00:34:53 +0700

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,12 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+};
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,6 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,700;1,100;1,700&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Feather Icons -->
     <script src="https://unpkg.com/feather-icons"></script>
 
